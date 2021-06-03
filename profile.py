0aa3d33ac1012afc52ec328084c02f3d56652517
Dustin Maas <dmaas@flux.utah.edu>
2021-06-02 23:15:37 -0600

    Fix. Remove old docs.

--- profile.py
+++ profile.py
@@ -8,95 +8,11 @@ import geni.urn as URN
 
 
 tourDescription = """
-
-# srsLTE Controlled RF
-
-Use this profile to intantiate an end-to-end LTE network in a controlled RF
-environment (wired connections between UE and eNB). The UE can be srsLTE-based
-or a Nexus 5.
-
-If you elect to use a Nexus 5, these nodes will be deployed:
-
-* Nexus 5 (`rue1`)
-* Generic Compute Node w/ ADB image (`adbnode`)
-* Intel NUC5300/B210 w/ srsLTE eNB/EPC (`enb1`)
-
-If instead you choose to use an srsLTE UE, these will be deployed:
-
-* Intel NUC5300/B210 w/ srsLTE (`rue1`) or
-* Intel NUC5300/B210 w/ srsLTE eNB/EPC (`enb1`)
+## srsRAN Handover
 
 """
 
 tourInstructions = """
-
-### Start EPC and eNB
-
-After your experiment becomes ready, login to `enb1` via `ssh` and do:
-
-```
-/local/repository/bin/start.sh
-```
-
-This will start a `tmux` session with three panes, running `srsepc` and
-`srsenb`, and then leaving your cursor in the last pane. After you've associated
-a UE with this eNB, you can use the third pane to run tests with `ping` or
-`iperf`. If you are not familiar with `tmux`, it's a terminal multiplexer that
-has some similarities to screen. Here's a [tmux cheat
-sheet](https://tmuxcheatsheet.com), but `ctrl-b o` (move to other pane) and
-`ctrl-b x` (kill pane), should get you pretty far. `ctrl-b d` will detach you
-from the `tmux` session and leave it running in the background. You can reattach
-with `tmux attach`.
-
-If you'd like to start `srsepc` and `srsenb` manually, here are the commands:
-
-```
-# start srsepc
-sudo srsepc /etc/srslte/epc.conf
-
-# start srsenb
-sudo srsenb /etc/srslte/enb.conf
-```
-
-### Nexus 5
-
-If you've deployed a Nexus 5, you should see it sync with the eNB eventually and
-obtain an IP address. Login to `adbnode` in order to access `rue1` via `adb`:
-
-```
-# on `adbnode`
-pnadb -a
-adb shell
-```
-
-Once you have an `adb` shell to `rue1`, you can use `ping` to test the
-connection, e.g.,
-
-```
-# in adb shell connected to rue1
-# ping SGi IP
-ping 172.16.0.1
-```
-
-If the Nexus 5 fails to sync with the eNB, try rebooting it via the `adb` shell.
-After reboot, you'll have to repeat the `pnadb -a` and `adb shell` commands on
-`adbnode` to reestablish a connection to the Nexus 5.
-
-### srsLTE UE
-
-If you've deployed an srsLTE UE, login to `rue1` and do:
-
-```
-/local/repository/bin/start.sh
-```
-
-This will start a `tmux` session with two panes: one running srsue and the other
-holding a spare terminal for running tests with `ping` and `iperf`. Again, if
-you'd like to run `srsue` manually, do:
-
-```
-sudo srsue /etc/srslte/ue.conf
-```
 
 """
 
@@ -190,11 +106,11 @@ link.vlan_tagging = True
 link.best_effort = True
 
 # Create RF links between the UE and eNodeBs
-rflink1 = request.RFLink1("rflink1")
+rflink1 = request.RFLink("rflink1")
 rflink1.addInterface(enb1_ue_rf)
 rflink1.addInterface(ue_enb1_rf)
 
-rflink2 = request.RFLink2("rflink2")
+rflink2 = request.RFLink("rflink2")
 rflink2.addInterface(enb2_ue_rf)
 rflink2.addInterface(ue_enb2_rf)
 
