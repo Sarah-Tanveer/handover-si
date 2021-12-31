a58a04b1298854894cc353a34a7d9c0d791955df
Ibrahim Moazzam <77812137+ibrahimmoazzam@users.noreply.github.com>
2021-12-31 15:07:39 +0500

    added comment

--- profile.py
+++ profile.py
@@ -9,6 +9,7 @@ import geni.urn as URN
 
 tourDescription = """
 ### srsRAN S1 Handover w/ Open5GS
+### Please work :(((
 
 This profile allocates resources in a controlled RF environment for
 experimenting with LTE handover. It deploys srsRAN on three nodes, each
