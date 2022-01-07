7260a81f87724940092438745576a85df704227b
Sarah-Tanveer <86280258+Sarah-Tanveer@users.noreply.github.com>
2022-01-07 22:53:18 +0500

    exited 100

--- profile.py
+++ profile.py
@@ -9,7 +9,7 @@ import geni.urn as URN
 
 tourDescription = """
 ### srsRAN S1 Handover w/ Open5GS
-### ioctl(TUNSETIFF): Device or resource busy
+### exited 100
 
 This profile allocates resources in a controlled RF environment for
 experimenting with LTE handover. It deploys srsRAN on three nodes, each
