5633d124f3803fc54da536726b5185ffdd30cea1
Sarah-Tanveer <86280258+Sarah-Tanveer@users.noreply.github.com>
2022-01-08 00:11:39 +0500

    permission issue

--- profile.py
+++ profile.py
@@ -10,7 +10,7 @@ import geni.urn as URN
 tourDescription = """
 ### srsRAN S1 Handover w/ Open5GS
 ### nteractions fixed i
-### git not being cloned fix??
+### permission issue?
 
 This profile allocates resources in a controlled RF environment for
 experimenting with LTE handover. It deploys srsRAN on three nodes, each
