35a98a40af6a4bf688338bf880a07ea841c738e6
Sarah-Tanveer <86280258+Sarah-Tanveer@users.noreply.github.com>
2022-01-07 23:08:16 +0500

    all interactions fixed

--- bin/deploy-open5gs.sh
+++ bin/deploy-open5gs.sh
@@ -19,7 +19,7 @@ sudo ip tuntap add name ogstun mode tun
 sudo ip addr add 10.45.0.1/16 dev ogstun
 sudo ip addr add 2001:230:cafe::1/48 dev ogstun
 sudo ip link set ogstun up
-sudo apt install python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev meson
+sudo apt -y install python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev meson
 git clone https://github.com/Sarah-Tanveer/open5gs
 cd open5gs
 meson build --prefix=`pwd`/install
